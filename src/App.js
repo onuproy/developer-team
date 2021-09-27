b68818e67a0dff4085d19ac5e97b052849dc88f6
onuproy <anupme01@gmail.com>
2021-09-27 21:40:57 +0600

    header update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, provident impedit quas odio, quidem facilis molestias voluptatibus possimus voluptates earum, adipisci rerum maxime sint perferendis saepe dolore cupiditate cumque similique!</p>
-      <p>
-        Anup
-      </p>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header-area">
+            <h1>Make An Company team</h1>
+            <p>Coder Item designs each theme from the end users perspective. We arrange the information as your visitors</p>
+            <strong>Generate UpTo 9000M</strong>
+        </div>
+    );
+};
+
+export default Header;
