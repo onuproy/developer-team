add60ba600626a88964917f55cbf96077897095e
onuproy <anupme01@gmail.com>
2021-10-03 19:05:56 +0600

    some issus fixed

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -3,7 +3,7 @@ import './Person.css'
 
 const Person = (props) => {
 
-    const {name, designation, id, salary, img} = props.person;
+    const {name, designation, id, salary, img, location} = props.person;
 
     return (
         <div className="single-person-item">
@@ -13,7 +13,8 @@ const Person = (props) => {
             <h2>Name: {name} </h2>
             <h4>Designation: {designation} </h4>
             <h4>Id: {id} </h4>
-            <h4>Salary {salary}</h4>
+            <h4>Salary: {salary}</h4>
+            <h4>Location: {location}</h4>
             <button onClick={() => props.handleAddToCart(props.person)}
                 className="bnt-regular"> <i class="fa fa-address-book" aria-hidden="true"></i> Add to cart</button>
         </div>
