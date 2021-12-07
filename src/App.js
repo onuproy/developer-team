7fa5cf12d302df9de1be6cf41f240c0237570927
Onup <anupme01@gmail.com>
2021-12-07 14:23:14 +0600

    index page update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 
-
-
 import Developer from './components/Developer/Developer';
 
 function App() {
